6f906b4aae15ca01b04c42ea466fc7cde3104050
ganzalizz <developer@proweb.by>
2010-12-06 15:03:35 +0200

    some

--- application/modules/pages/controllers/IndexController.php
+++ application/modules/pages/controllers/IndexController.php
@@ -46,6 +46,7 @@ class pages_IndexController extends Zend_Controller_Action {
         $this->layout->current_type = 'pages';
         $this->layout->lang = $this->lang;
         $this->layout->page = $this->_page;
+        
        
         
         
